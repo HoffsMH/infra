812f9e441059412c7e2543a7efc61d37cee49914
HoffsMH <matthecker@pm.me>
2022-08-09 17:14:42 -0500

    misc things for importing dotfiles repo

---
+++ assets/workstation/pre-abandon-report.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo " "
+echo "~/personal/media/video/capture"
+exa -la ~/personal/media/video/capture
+
+echo ""
+echo "~/personal/media/audio/capture"
+exa -la ~/personal/media/audio/capture
+
+echo ""
+echo "~/personal/media/image/capture"
+exa -la ~/personal/media/image/capture
+
+echo " "
+echo "installed without dotfiles:"
+cat ~/.installed
+
+
+echo ""
+gfold $HOME
