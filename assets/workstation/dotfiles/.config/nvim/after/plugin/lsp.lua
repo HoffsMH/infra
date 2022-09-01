274d6976a05646f7a643ca2e404b10a0091a1705
HoffsMH <matthecker@pm.me>
2022-09-01 17:34:36 -0500

    no more tabnine

--- assets/workstation/dotfiles/.config/nvim/after/plugin/lsp.lua
+++ assets/workstation/dotfiles/.config/nvim/after/plugin/lsp.lua
@@ -7,18 +7,9 @@ local source_mapping = {
 	buffer = "[Buffer]",
 	nvim_lsp = "[LSP]",
 	nvim_lua = "[Lua]",
-	cmp_tabnine = "[TN]",
 	path = "[Path]",
 }
 
-local tabnine = require("cmp_tabnine.config")
-tabnine:setup({
-	max_lines = 1000,
-	max_num_results = 4,
-	sort = true,
-	run_on_every_keystroke = true,
-	snippet_placeholder = "..",
-})
 
 local function config(_config)
 	return vim.tbl_deep_extend("force", {
@@ -57,12 +48,6 @@ cmp.setup({
 		format = function(entry, vim_item)
 			vim_item.kind = lspkind.presets.default[vim_item.kind]
 			local menu = source_mapping[entry.source.name]
-			if entry.source.name == "cmp_tabnine" then
-				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-					menu = entry.completion_item.data.detail .. " " .. menu
-				end
-				vim_item.kind = ""
-			end
 			vim_item.menu = menu
 			return vim_item
 		end,
@@ -70,7 +55,6 @@ cmp.setup({
 
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
-		{ name = "cmp_tabnine" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
   })
