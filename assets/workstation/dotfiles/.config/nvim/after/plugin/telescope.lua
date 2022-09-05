38271c2547fbd5fc904a3bb7804fbf25162e1429
HoffsMH <matthecker@pm.me>
2022-09-05 12:58:16 -0500

    ok

--- assets/workstation/dotfiles/.config/nvim/after/plugin/telescope.lua
+++ assets/workstation/dotfiles/.config/nvim/after/plugin/telescope.lua
@@ -15,4 +15,23 @@ require('telescope').setup{
       '.gitignore',
     },
   },
+  pickers = {
+      find_files = {
+        mappings = {
+          n = {
+            ["cdo"] = function(prompt_bufnr)
+              require("telescope.actions").close(prompt_bufnr)
+              vim.cmd(string.format("silent lcd %s", ".."))
+            end,
+
+            ["cdn"] = function(prompt_bufnr)
+                local selection = require("telescope.actions.state").get_selected_entry()
+                local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+                require("telescope.actions").close(prompt_bufnr)
+                vim.cmd(string.format("silent lcd %s", dir))
+            end
+          },
+        },
+      },
+    },
 }
