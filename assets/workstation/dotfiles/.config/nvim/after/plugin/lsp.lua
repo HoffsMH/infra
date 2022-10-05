92439c0ef4cf8d9ad4cc3c200fcc59880185c10a
HoffsMH <matthecker@pm.me>
2022-10-04 19:00:05 -0500

    go imports

--- assets/workstation/dotfiles/.config/nvim/after/plugin/lsp.lua
+++ assets/workstation/dotfiles/.config/nvim/after/plugin/lsp.lua
@@ -74,3 +74,20 @@ lspconfig.eslint.setup(config({
 require'lspconfig'.ember.setup(config())
 require'lspconfig'.solargraph.setup(config())
 require'lspconfig'.gopls.setup(config())
+
+
+function go_org_imports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for cid, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
+      end
+    end
+  end
+end
+
+vim.api.nvim_command([[autocmd BufWritePre *.go lua go_org_imports()]])
