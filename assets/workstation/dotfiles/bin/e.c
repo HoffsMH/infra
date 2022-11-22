1743699adbda0a0e75d1753c60e55cc37746274e
HoffsMH <matthecker@pm.me>
2022-11-22 11:58:58 -0600

    no more e.c

--- assets/workstation/dotfiles/bin/e.c
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This removes a newline at end of file only if it is a new line
-# https://stackoverflow.com/questions/28259288/removing-newline-at-end-of-file-using-bash-shell-script
-#sed -i.bak '/^[[:blank:]]*$/{$d;}' $1
-
-#echo -e "## ${2:-cap}.md\n" >> $1
-
-nvim -c $ $1
