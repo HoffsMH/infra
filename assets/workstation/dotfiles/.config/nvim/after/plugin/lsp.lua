9f843d7f6566feaf702c9303c704212111e8ee1f
HoffsMH <matthecker@pm.me>
2022-12-22 14:13:07 -0600

    ok

--- assets/workstation/dotfiles/.config/nvim/after/plugin/lsp.lua
+++ assets/workstation/dotfiles/.config/nvim/after/plugin/lsp.lua
@@ -59,9 +59,9 @@ cmp.setup({
 	},
 
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp", keyword_length = 3 },
-		{ name = "luasnip" },
-		{ name = "buffer", keyword_length = 5 },
+		{ name = "nvim_lsp", keyword_length = 2 },
+		{ name = "luasnip", keyword_length = 1 },
+		{ name = "buffer", keyword_length = 2 },
   })
 })
 
