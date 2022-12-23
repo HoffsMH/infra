5dbe112b82c10b66a8a0c592e554f781adc28703
HoffsMH <matthecker@pm.me>
2022-12-23 12:26:14 -0600

    hidden true for find_opts

--- assets/workstation/dotfiles/.config/nvim/lua/hoffs/telescope-project-files.lua
+++ assets/workstation/dotfiles/.config/nvim/lua/hoffs/telescope-project-files.lua
@@ -4,7 +4,7 @@ local vim = vim
 
 M.project_files = function()
   local git_opts = { show_untracked = true } -- define here if you want to define something
-  local find_opts = {} -- define here if you want to define something
+  local find_opts = { hidden=true } -- define here if you want to define something
   vim.fn.system('git rev-parse --is-inside-work-tree')
   if vim.v.shell_error == 0 then
     require"telescope.builtin".git_files(git_opts)
