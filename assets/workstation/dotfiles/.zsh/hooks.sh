46a74592622bb6c529bd1718fee6940bca1ccd9c
HoffsMH <matthecker@pm.me>
2023-03-21 13:33:17 -0500

    zoxide i options have changed

--- assets/workstation/dotfiles/.zsh/hooks.sh
+++ assets/workstation/dotfiles/.zsh/hooks.sh
@@ -8,6 +8,8 @@ compinit
 
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
+# zoxide options
+export _ZO_FZF_OPTS="--algo=v2 --no-exact --layout=reverse"
 
 # unset z
 unalias z
@@ -17,3 +19,4 @@ export LDFLAGS="-L/usr/local/opt/zlib/lib"
 export CPPFLAGS="-I/usr/local/opt/zlib/include"
 export PKG_CONFIG_PATH="/usr/local/opt/zlib/lib/pkgconfig"
 export KEEP_BUILD_PATH=true
+
