6f0bc44ddb32b40525d5bc67d9b5bca2d0f0ce5a
HoffsMH <matthecker@pm.me>
2023-06-12 15:21:53 -0500

    ok

--- assets/workstation/dotfiles/.config/nvim/after/plugin/telescope.lua
+++ assets/workstation/dotfiles/.config/nvim/after/plugin/telescope.lua
@@ -10,18 +10,22 @@ changed_on_branch = function()
     local pickers = require('telescope.pickers')
     local sorters = require('telescope.sorters')
     local finders = require('telescope.finders')
+    local devicons = require"nvim-web-devicons"
 
     pickers.new {
+        -- one day get dev icons working on this
+        devicons = devicons,
         results_title = 'Modified on current branch',
         finder = finders.new_oneshot_job({'/home/hoffs/bin/g.diffed-files'}),
         sorter = sorters.get_fuzzy_file(),
-        previewer = previewers.new_termopen_previewer {
-            get_command = function(entry)
-                return {'bat', entry.value}
+        previewer = previewers.vim_buffer_cat.new {
+            get_buffer_by_name = function(_, entry)
+                return entry.value
             end
-        },
+        }
     }:find()
 end
+
 
 require('telescope').setup{
   defaults = {
