20a91c80f83828a86ec15ae22f52b9cefc3caa19
HoffsMH <matthecker@pm.me>
2023-07-09 12:21:27 -0500

    no more luasnip

--- assets/workstation/dotfiles/.config/nvim/lua/hoffs/packer.lua
+++ assets/workstation/dotfiles/.config/nvim/lua/hoffs/packer.lua
@@ -71,7 +71,6 @@ require('packer').startup(function(use)
   use("onsails/lspkind-nvim")
 
   use("L3MON4D3/LuaSnip")
-  use("saadparwaiz1/cmp_luasnip")
   use("hrsh7th/cmp-nvim-lsp")
   use("hrsh7th/cmp-buffer")
   use('jvgrootveld/telescope-zoxide')
