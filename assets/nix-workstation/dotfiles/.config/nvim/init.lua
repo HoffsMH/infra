bb13cd20f3823e28a8490b90198c3bf3e61d6586
HoffsMH <matthecker@pm.me>
2024-05-13 10:55:23 -0500

    .

--- assets/nix-workstation/dotfiles/.config/nvim/init.lua
+++ assets/nix-workstation/dotfiles/.config/nvim/init.lua
@@ -279,9 +279,25 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+
+  'andymass/vim-matchup',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
   'onsails/lspkind-nvim',
 
   -- NOTE: Plugins can also be added by using a table,
